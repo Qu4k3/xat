12d43de9bda08caaacd2fff8ce88e0e6c69f9804
paul.guillamon@gmail.com <darksector>
2017-04-03 19:47:52 +0200

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,15 @@
 function searchUser() {
-    // Declare variables
-    var input, filter, ul, li, a, i;
-    input = document.getElementById('inputSearch');
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("usersList");
-    li = ul.getElementsByTagName('li');
 
+    var input = document.getElementById('inputSearch');
+    var filter = input.value.toUpperCase();
+    var ul = document.getElementById("usersList");
+    var li = ul.getElementsByTagName('li');
 
     $("#usersList").show();
 
     // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
+    for (var i = 0; i < li.length; i++) {
+        var a = li[i].getElementsByTagName("a")[0];
         if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
             li[i].style.display = "";
         } else {
--- private/messages.php
+++ private/messages.php
@@ -2,11 +2,10 @@
 
     include 'db_con.php';
 
-
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
          if (isset($_POST["send"])){                            
-            if (!empty($_POST["inputMessage"])) { 
-                
+            if (!empty($_POST["inputMessage"])) {
+
                 $sql = "INSERT INTO Messages (text, sender, receiver, created_at) 
 
                 VALUES (
